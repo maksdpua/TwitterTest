d0a87b9b7afe5399cbdc0af49a36a8f837f5bb64
Maks <maksdpua@gmail.com>
2016-05-25 20:19:39 +0300

    7

--- TwitterTest/TwitterClient.swift
+++ TwitterTest/TwitterClient.swift
@@ -13,5 +13,47 @@ import BDBOAuth1Manager
 class TwitterClient: BDBOAuth1SessionManager {
     //OAuth = Fetch Request Token + Redirect to Auth + Fetch Access Token + Callback
     
-    static let sharedInstance = TwitterClient(baseURL: NSURL(string: "https://publish.twitter.com"), consumerKey: "aCaciS7xZDNkAPXPCazgbNq2R", consumerSecret: "opNdQGUHSEVeu9qbYs9wmcMOPXxy8d5V518cO1ZLtaA3qtUaHr")
+    static let sharedInstance = TwitterClient(baseURL: NSURL(string: "https://api.twitter.com"), consumerKey: "aCaciS7xZDNkAPXPCazgbNq2R", consumerSecret: "opNdQGUHSEVeu9qbYs9wmcMOPXxy8d5V518cO1ZLtaA3qtUaHr")
+    
+    var loginSuccess: (()->())?
+    var loginFailure: ((NSError)->())?
+    
+    //Getting request token to open up auth link in safari
+    func login(success: ()->(), failure: (NSError) -> () ) {
+        loginSuccess = success
+        loginFailure = failure
+        
+        deauthorize()
+        
+        fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "twittertest://oauth"), scope: nil, success: { (requestToken) in
+            print("Got token")
+            
+            let url = NSURL(string: "https://api.twitter.com/oauth/authenticate?oauth_token" + requestToken.token)!
+            
+            UIApplication.sharedApplication().openURL(url)
+            }) { (error) in
+                print("Error: \(error.localizedDescription)")
+                self.loginFailure?(error)
+                
+        }
+    }
+    //get access token and save user
+    func handleOpenURL(url: NSURL) {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        appDelegate.splashDeleay = true
+        
+        let requestToken = BDBOAuth1Credential(queryString: url.query)!
+        
+        //Getting access token
+        fetchAccessTokenWithPath("oauth/access_token", method: "POST", requestToken: requestToken, success: { (accessToken) in
+                self.loginSuccess?()
+            // TODO:
+            }) { (error) in
+                print("Error: \(error.localizedDescription)")
+                self.loginFailure?(error)
+        }
+    }
+    
+    
 }
